12cf10f919fff253da738cc115525bdd07569938
Roman Pogorelov <rmk140987@gmail.com>
2020-06-30 18:00:31 +0300

    9. JS Intro - Fix js. Add check found elements.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 let modalShowClass = "modal-show";
 
 let mapOpenButton = document.querySelector(".map-open-button");
-let mapPopupSelector = ".map-popup";
-let mapCloseButton = document.querySelector(mapPopupSelector + " .close-popup-window");
+if (mapOpenButton) {
+  let mapPopupSelector = ".map-popup";
+  let mapCloseButton = document.querySelector(mapPopupSelector + " .close-popup-window");
 
-function toggleMapPopup() {
-  document.querySelector(mapPopupSelector).classList.toggle(modalShowClass);
+  function toggleMapPopup() {
+    document.querySelector(mapPopupSelector).classList.toggle(modalShowClass);
+  }
+
+  mapOpenButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    toggleMapPopup();
+  });
+  mapCloseButton.addEventListener("click", toggleMapPopup);
 }
-
-mapOpenButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  toggleMapPopup();
-});
-mapCloseButton.addEventListener("click", toggleMapPopup);
 
 let buyOpenButtons = document.querySelectorAll(".buy-item-button");
 let buyPopupSelector = ".product-popup";
@@ -31,45 +33,48 @@ buyOpenButtons.forEach(element => {
 buyCloseButton.addEventListener("click", toggleBuyPopup);
 
 let writeToUsOpenButton = document.querySelector(".write-to-us-button");
-let writeToUsPopupSelector = ".write-to-us-popup";
-let writeToUsCloseButton = document.querySelector(writeToUsPopupSelector + " .close-popup-window");
+if (writeToUsOpenButton) {
 
-function toggleWriteToUsPopup() {
-  document.querySelector(writeToUsPopupSelector).classList.toggle(modalShowClass);
+  let writeToUsPopupSelector = ".write-to-us-popup";
+  let writeToUsCloseButton = document.querySelector(writeToUsPopupSelector + " .close-popup-window");
+
+  function toggleWriteToUsPopup() {
+    document.querySelector(writeToUsPopupSelector).classList.toggle(modalShowClass);
+  }
+
+  let isStorageSupport;
+  let name = "";
+  let email = "";
+
+  let writeToUsForm = document.querySelector(writeToUsPopupSelector + " form");
+  let nameInput = writeToUsForm.querySelector("#name-field");
+  let emailInput = writeToUsForm.querySelector("#email-field");
+  writeToUsOpenButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    toggleWriteToUsPopup();
+    try {
+      name = localStorage.getItem("name");
+      email = localStorage.getItem("email");
+      isStorageSupport = true;
+    } catch (e) {
+      isStorageSupport = false;
+    }
+    if (isStorageSupport) {
+      if (name) {
+        nameInput.value = name;
+      }
+      if (email) {
+        emailInput.value = email;
+      }
+    }
+  });
+  writeToUsCloseButton.addEventListener("click", toggleWriteToUsPopup);
+  writeToUsForm.addEventListener("submit", function (event) {
+    let nameValue = nameInput.value;
+    let emailValue = emailInput.value;
+    if (nameValue && emailValue && isStorageSupport) {
+      localStorage.setItem("name", nameValue);
+      localStorage.setItem("email", emailValue);
+    }
+  })
 }
-
-let isStorageSupport;
-let name = "";
-let email = "";
-
-let writeToUsForm = document.querySelector(writeToUsPopupSelector + " form");
-let nameInput = writeToUsForm.querySelector("#name-field");
-let emailInput = writeToUsForm.querySelector("#email-field");
-writeToUsOpenButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  toggleWriteToUsPopup();
-  try {
-    name = localStorage.getItem("name");
-    email = localStorage.getItem("email");
-    isStorageSupport = true;
-  } catch (e) {
-    isStorageSupport = false;
-  }
-  if (isStorageSupport) {
-    if (name) {
-      nameInput.value = name;
-    }
-    if (email) {
-      emailInput.value = email;
-    }
-  }
-});
-writeToUsCloseButton.addEventListener("click", toggleWriteToUsPopup);
-writeToUsForm.addEventListener("submit", function (event) {
-  let nameValue = nameInput.value;
-  let emailValue = emailInput.value;
-  if (nameValue && emailValue && isStorageSupport) {
-    localStorage.setItem("name", nameValue);
-    localStorage.setItem("email", emailValue);
-  }
-})
